b805e5954c5846866057240e71cb2f3cc91627eb
Wil.i.am <wiliamlee@sas.com.br>
2021-02-23 21:35:52 -0300

    feat: add class and pass to children body

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Transferência'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Transferência'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+          body: Column(
+            children: <Widget>[
+              ItemTransferencia(Transferencia(100, 120202)),
+              ItemTransferencia(Transferencia(2999, 4949494)),
+              ItemTransferencia(Transferencia(399, 4949494)),
+            ],
+          )),
     ));
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _tranferencia;
+
+  ItemTransferencia(this._tranferencia);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_tranferencia.valor.toString()),
+          subtitle: Text(_tranferencia.numeroConta.toString())),
+    );
+  }
+}
